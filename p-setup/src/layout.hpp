e898292525b2ccdd732c79573337ea8fa62b32cd
eric <ericguerra141@gmail.com>
2025-06-04 20:05:54 -0400

    Added code comments, and created a Dev directory generator.

--- p-setup/src/layout.hpp
+++ p-setup/src/layout.hpp
@@ -5,11 +5,25 @@ using namespace std;
 class termStr
 {
     public:
-    string pName;
-    string mkdir_pName;
-    string touch_pName;
-    string list_pDir;
-    string ovsc_pName;
+    string sys_User; // System username.
+    string pName; // Project name.
+    string install_Dir; // 'Dev' folder directory.
+    string mkdir_Dev; // Command that generates the 'Dev' directory.
+    
+    // Commands for generating a project directory.
+        string mkdir_pName; // Creates the project folders.
+        string touch_pName; // Creates the project source code file.
+        string list_pDir; // Lists the created directory and its contents in the terminal. --For debugging
+    
+    // Opens the project directory in Visual Studio Code. --Example: 'code projectdir' 
+        string ovsc_pName;
+
+    void gen_devDir()
+    {
+
+        mkdir_Dev = "mkdir /home/" + sys_User + "/Dev";
+        system(mkdir_Dev.c_str());
+    }
     
     void gen_proj()
     {
@@ -44,4 +58,16 @@ void main_app()
     new_proj.list_proj();
     new_proj.open_proj();
     exit(0);
+}
+
+void prgrm_Setup() // WARNING! Only tested on Ubuntu 22.04 LTS using the ext4 filesystem.
+{
+    termStr setup;
+
+    system("clear");
+    cout << "[Simple Project Manager v0.1 Setup]\n\n";
+    cout << "System Username: ";
+    cin >> setup.sys_User;
+    setup.gen_devDir();
+    cout << "Place ps.o inside 'Dev' directory."; // This is very temporary.
 }
--- p-setup/src/ps.cpp
+++ p-setup/src/ps.cpp
@@ -7,22 +7,23 @@ char startM;
 int main()
 {
     // Create project...
+    system("clear");
     cout << "[Simple Project Manager v0.1]\n\n";
     cout << "[1] Start\n";
-    //cout << "[2] Debug\n";
+    cout << "[2] Setup\n";
     cout << "[X] Close\n";
     cout << "Choice... ";
     cin >> startM;
     switch(startM)
     {
         case('1'):
-            main_app();
+            main_app(); // Main program.
             break;
-        /*case('2'):
-            dMode();
-            break;*/
-        case('X'):
-            exit(0);
+        case('2'):
+            prgrm_Setup(); // Experimental.
+            break;
+        case('X' || 'x'):
+            exit(0); // Safely exits the program.
         default:
             cerr << "Invalid Input, please select a valid character.";
             break;
